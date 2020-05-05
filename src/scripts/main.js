abe11b33782135298132507b7581360a5c2c15d3
Kirk Sudduth <kirksudduth@gmail.com>
2020-05-05 11:55:17 -0500

    got it working thanks to zach nicholson

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,21 @@
 console.log("Keepit whunhunnid");
-import API from "./data.js";
+// import API from "./data.js";
+import { artistPush, createArtist } from "./createArtist.js";
 const jumpStop = [];
 const polar = [];
 const chatten = [];
 
-API.getArtists();
+console.log("Artist Created: ", createArtist("Tinsel Green", "pop", 57));
+artistPush(createArtist("Bruce Atikins", "country", 23));
+artistPush(createArtist("Jensen Brown", "pop", 20));
+artistPush(createArtist("Dre Funkz", "funk", 25));
+artistPush(createArtist("Dusta Grimes", "rap", 21));
+artistPush(createArtist("Bartholomew Danielson", "bluegrass", 23));
+artistPush(createArtist("Avilee Dallas", "country", 19));
+artistPush(createArtist("Austin Kinkaid", "pop", 22));
+artistPush(createArtist("Loyoncé Branis", "rap", 27));
+
+// API.getArtists();
 console.log("JumpStop: ", jumpStop);
 console.log("Polar: ", polar);
 console.log("Chatten: ", chatten);
